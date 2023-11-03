aa915de26ec6c0439ae09ac3ebeafda1d355f007
oakley-Thomas <73431617+oakley-Thomas@users.noreply.github.com>
2023-11-03 14:31:59 -0400

    Added basic post information to the database, set up endpoint to receive quiz history updates, added comments on sections that need improving

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ client = MongoClient("mongodb://mongo:27017/")
 db = client["CSE312ProjectDB"]
 registered_users = db["users"]
 posts_collection = db["posts"]
+quiz_collection = db["quizzes"]
 
 
 # @app.route('/', methods=['POST'])
@@ -47,11 +48,11 @@ def home():
             response.set_cookie('username', nametest)
         return response
     return render_template('index.html')
+
 @app.route('/login')
 def login():
     print("Redirecting to Login Page")
     return render_template("login.html")
-
 
 @app.route('/login-request', methods=['POST'])
 def loginRequest():
@@ -79,20 +80,33 @@ def posts():
 
 @app.route('/create-quiz')
 def load_quiz_creator():
-    print("Creating Quiz!")
     return render_template("quiz_creator.html")
+
+@app.route('/quiz-history')
+def quiz_history():
+    all_quizzes = json.dumps(list(quiz_collection.find({"title": {"$exists": "true"}})), default=str)
+    return all_quizzes
 
 @app.route('/submit-quiz', methods=['POST'])
 def submit_quiz():
+    #if request.cookies.get("auth-token") == None:
+     #   return
     post = request.get_json(force=True)
+    # Get username and create database: Username -> Question -> Title, Description, Answers, Correct
+    post["title"] = html.escape(post["title"])
+    # TODO: Ask ChatGPT if its a valid question?
+    # TODO: Escape each input answer HTML
     post["correct"] = html.escape(post["correct"])
-    print("-----------------------", flush=True)
-    print(post["correct"], flush=True)
-    print("-----------------------", flush=True)
-    """post["Description"] = html.escape(post["Description"])
-    post["Correct"] = html.escape(post["Correct"])
-    print(post["Correct"])
-    print("------------------")"""
+
+    #---- Database ---
+    quiz = {
+        "title": post["title"],
+        "choices": post["choices"],
+        "answer": post["correct"]
+    }
+    jsonQuiz = json.dumps(quiz)
+    # For now the uid for the post is just the title.... this probably means no duplicate questions
+    quiz_collection.insert_one({post["title"]: jsonQuiz})
     return render_template("quiz_creator.html")
 
 @app.route('/createPost', methods=['POST'])
--- static/js/quiz_creator.js
+++ static/js/quiz_creator.js
@@ -1,18 +1,37 @@
 function postData() {
+    // Extract title
     const title = document.getElementById("Title").value;
-    const description = document.getElementById("Description").value;
+
+    // Extract all choices and json encode
     const userAnswers = document.getElementsByName("user-answer");
-    var correct = "TEST";
+
+    // Extract the correct answer
+    var correct = "NONE";
+    const choices = {}
     for (var i = 0; i < userAnswers.length; i++) {
+        // Store "option1 : option1-text.value
+        choice = userAnswers[i].id; // option0
+        choiceName = choice + "-text";
+        choiceValue = document.getElementById(choiceName).value;
+        choices[choice] = choiceValue;
+
+        console.log(choice + ": " + choiceValue)
         if (userAnswers[i].checked){
             console.log("Correct Answer: " + userAnswers[i].value)
             correct = userAnswers[i].value
         }
     }
+    JSON.stringify(choices);
 
-    const post = { title: title, description: description, correct: correct };
-    axios.post('/submit-quiz', post)
-        .then(response => console.log(response.data))
-        .catch(error => console.error(error));
+    if (correct != "NONE"){
+        const post = { title: title, correct: correct, choices: choices };
+        axios.post('/submit-quiz', post)
+                .then(response => console.log(response.data))
+                .catch(error => console.error(error));
+    }
+    else{
+        alert("Please select the correct answer");
+    }
+
 }
 
