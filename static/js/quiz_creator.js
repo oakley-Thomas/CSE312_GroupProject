2c763493d4663f9e770ebc8806bb00c6f9daac33
Ken Yu <104584223+Ken-256@users.noreply.github.com>
2023-11-09 15:49:51 -0500

    Update quiz_creator.js
    added image upload  functionality to quiz creation

--- static/js/quiz_creator.js
+++ static/js/quiz_creator.js
@@ -1,9 +1,25 @@
-function postData() {
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+async function postData() {
     // Extract title
     const title = document.getElementById("Title").value;
 
     // Extract all choices and json encode
     const userAnswers = document.getElementsByName("user-answer");
+
+    // add image
+    const imageFile = document.getElementById("image").files[0];
+    let image;
+    if (imageFile) {
+        image = await getBase64(imageFile);
+    }
 
     // Extract the correct answer
     var correct = "NONE";
@@ -24,7 +40,10 @@ function postData() {
     JSON.stringify(choices);
 
     if (correct != "NONE"){
-        const post = { title: title, correct: correct, choices: choices };
+        const post = { title: title, correct: correct, choices: choices};
+        if (image) {
+            post.image = image;
+        }
         axios.post('/submit-quiz', post)
                 .then(response => console.log(response.data))
                 .catch(error => console.error(error));
