3e454cb552cd8221379b941013ecb84de6f3f56a
Ken Yu <104584223+Ken-256@users.noreply.github.com>
2023-11-10 17:59:55 -0500

    Update quiz_creator.js
    updated to match dev while adding image feature

--- static/js/quiz_creator.js
+++ static/js/quiz_creator.js
@@ -21,6 +21,7 @@ async function postData() {
         image = await getBase64(imageFile);
     }
 
+
     // Extract the correct answer
     var correct = "NONE";
     const choices = {}
@@ -30,27 +31,94 @@ async function postData() {
         choiceName = choice + "-text";
         choiceValue = document.getElementById(choiceName).value;
         choices[choice] = choiceValue;
-
-        console.log(choice + ": " + choiceValue)
         if (userAnswers[i].checked){
-            console.log("Correct Answer: " + userAnswers[i].value)
             correct = userAnswers[i].value
         }
     }
+
+    // Extract the duration
+    const duration = document.getElementById("quizDuration").value;
+
     JSON.stringify(choices);
 
     if (correct != "NONE"){
-        const post = { title: title, correct: correct, choices: choices};
+        const post = { title: title, correct: correct, choices: choices, duration: duration};
         if (image) {
             post.image = image;
         }
         axios.post('/submit-quiz', post)
-                .then(response => console.log(response.data))
+                .then(response => confirmSubmission())
                 .catch(error => console.error(error));
     }
     else{
         alert("Please select the correct answer");
     }
+    updatePostHistory();
+}
+
+function showQuizCreator()
+{
+    // Set the HTML
+    document.getElementById("postInput").innerHTML = 
+    `
+    <form id="myForm">
+    <input class="inputTitle" type="text" name="Title" id="Title" placeholder="Title: "><br>
+    <div class="formLine"></div>
+    <div class="multipleChoiceOptions">
+        <!-- Choice 1 -->
+        <input type="radio" id="option1" name="user-answer" value="option1">
+        <label for="option1">
+            <input class="user-entered-answer" type="text" name="option1-text" id="option1-text" placeholder="Enter answer here">
+        </label><br>
+        <!-- Choice 2 -->
+        <input type="radio" id="option2" name="user-answer" value="option2">
+        <label for="option2">
+            <input class="user-entered-answer" type="text" name="option2-text" id="option2-text" placeholder="Enter answer here">
+        </label><br>
+        <!-- Choice 3 -->
+        <input type="radio" id="option3" name="user-answer" value="option3">
+        <label for="option3">
+            <input class="user-entered-answer" type="text" name="option3-text" id="option3-text" placeholder="Enter answer here">
+        </label><br>
+        <!-- Image upload function -->
+            <input type="file" name="image" id="image">
+        <br>
+        <br>
+        <div class="timer">
+            <label for="quizDuration">Duration:</label>
+            <select name="duration" id="quizDuration">
+                <option value="1">1 Hour</option>
+                <option value="6">6 Hours</option>
+                <option value="12">12 Hours</option>
+                <option value="24">24 Hours</option>
+            </select>
+        </div>
+    </div>
+    </form>`;
+    // Change the button text and onclick functionality
+    document.getElementById("sendButton").value = "Submit";
+    document.getElementById("sendButton").onclick = function() { postData() }
+    // Change back button onclick
+    document.getElementById("backButton").onclick = function() { hideQuizCreator() }
 
 }
 
+function hideQuizCreator(inResponse)
+{
+    document.getElementById("postInput").innerHTML = '<h1>Welcome!</h1>'
+    // Send button
+    document.getElementById("sendButton").value = "Create Quiz!";
+    document.getElementById("sendButton").onclick = function() { showQuizCreator() }
+
+    document.getElementById("backButton").onclick = function() { returnHome() }
+}
+
+function returnHome() {
+    location.replace("/");
+}
+
+function confirmSubmission()
+{
+    document.getElementById("postInput").innerHTML = '<h1>Submitted!</h1>'
+    setTimeout(function() { hideQuizCreator() }, 2500);
+}
